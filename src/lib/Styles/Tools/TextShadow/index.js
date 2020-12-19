e75657beb8dbc335b75660717a6deec3ceb3f812
Marco Bruno <marco.bruno.br@gmail.com>
2020-12-19 19:52:03 +0100

    :lipstick: Change px to rem in TextShadow

--- src/lib/Styles/Tools/TextShadow/index.js
+++ src/lib/Styles/Tools/TextShadow/index.js
@@ -1,11 +1,11 @@
 import { css } from 'styled-components'
 
 const small = css`
-  text-shadow: 5px 5px 0px var(--color-black-first);
+  text-shadow: 0.5rem 0.5rem 0px var(--color-black-first);
 `
 
 const medium = css`
-  text-shadow: 10px 10px 0px var(--color-black-first);
+  text-shadow: 1rem 1rem 0px var(--color-black-first);
 `
 
 export const TextShadow = {
