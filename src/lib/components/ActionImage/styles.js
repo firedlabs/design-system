29a9efd0cb008338fc48f60ab96dae89ad7e99a4
Marco Bruno <marco.bruno.br@gmail.com>
2020-12-20 18:37:44 +0100

    :lipstick: ActionImage is reponsive

--- src/lib/components/ActionImage/styles.js
+++ src/lib/components/ActionImage/styles.js
@@ -1,11 +1,14 @@
 import styled from 'styled-components'
 import { BoxShadow } from '../../Styles/Tools'
 
-export const Image = styled.img``
+export const Image = styled.img`
+  width: 100%;
+`
 
 export const Action = styled.a`
   display: inline-block;
   cursor: pointer;
+  width: 20rem;
   ${BoxShadow.medium};
 
   &:hover {
