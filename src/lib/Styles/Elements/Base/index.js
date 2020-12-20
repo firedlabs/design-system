c7d84b2ddda9f5ba490c06f00643f7e70dbd3673
Marco Bruno <marco.bruno.br@gmail.com>
2020-12-20 18:10:36 +0100

    :lipstick: Add braekpoint 510 and 430 to font-size in Base

--- src/lib/Styles/Elements/Base/index.js
+++ src/lib/Styles/Elements/Base/index.js
@@ -4,11 +4,17 @@ const Base = createGlobalStyle`
   html, body {
     font-family: 'Fira Code', sans-serif;
     font-size: 10px;
-  }
 
-  @media(max-width: 834px) {
-    html, body {
-      font-size: 7px;
+    @media(max-width: 834px) {
+      font-size: 7px
+    }
+
+    @media(max-width: 510px) {
+      font-size: 6px;
+    }
+
+    @media(max-width: 430px) {
+      font-size: 5px;
     }
   }
 `
