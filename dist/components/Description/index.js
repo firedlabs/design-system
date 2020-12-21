94a19e699246fb31d5114fa2e02cd9b4dfe632b4
Marco Bruno <marco.bruno.br@gmail.com>
2020-12-21 16:30:21 +0100

    :bookmark: Version 0.2.27

--- dist/components/Description/index.js
+++ dist/components/Description/index.js
@@ -16,7 +16,7 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _templateObject2() {
-  var data = _taggedTemplateLiteral(["\n  ", ";\n  color: var(--color-third);\n  ", ";\n"]);
+  var data = _taggedTemplateLiteral(["\n  ", ";\n  color: var(--color-third);\n  ", ";\n\n  & > strong {\n    font-weight: bold;\n  }\n\n  & > a {\n    color: var(--color-first);\n    font-weight: bold;\n    text-underline-offset: var(--gap-smallest);\n    text-decoration-thickness: 3px;\n  }\n"]);
 
   _templateObject2 = function _templateObject2() {
     return data;
