5a71e52e4936a3d5d45146c837ed421b5a067b75
Marco Bruno <marco.bruno.br@gmail.com>
2020-12-21 01:01:28 +0100

    :lipstick: Add margin-bottom in Heading with 4 options

--- src/lib/components/Heading/index.js
+++ src/lib/components/Heading/index.js
@@ -1,11 +1,34 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { Heading as toolsHeading, TextShadow } from '../../Styles/Tools'
+
+const hasMedium = ({ medium }) =>
+  medium &&
+  css`
+    margin-bottom: var(--gap-medium);
+  `
+
+const hasBig = ({ big }) =>
+  big &&
+  css`
+    margin-bottom: var(--gap-big);
+  `
+
+const hasBiggest = ({ biggest }) =>
+  biggest &&
+  css`
+    margin-bottom: var(--gap-biggest);
+  `
 
 const Heading = styled.h1`
   ${toolsHeading};
   color: var(--color-third);
   ${TextShadow.small};
   text-align: center;
+  margin-bottom: var(--gap-small);
+
+  ${hasMedium};
+  ${hasBig};
+  ${hasBiggest};
 `
 
 export default Heading
--- src/stories/Heading.stories.js
+++ src/stories/Heading.stories.js
@@ -10,5 +10,32 @@ const Template = (args) => <Heading {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-  children: 'Curso Feliz de HTML e CSS'
+  children: 'Curso Feliz de HTML e CSS',
+  medium: false,
+  big: false,
+  biggest: false
+}
+
+export const Medium = Template.bind({})
+Medium.args = {
+  children: 'Curso Feliz de HTML e CSS',
+  medium: true,
+  big: false,
+  biggest: false
+}
+
+export const Big = Template.bind({})
+Big.args = {
+  children: 'Curso Feliz de HTML e CSS',
+  medium: false,
+  big: true,
+  biggest: false
+}
+
+export const Biggest = Template.bind({})
+Biggest.args = {
+  children: 'Curso Feliz de HTML e CSS',
+  medium: false,
+  big: false,
+  biggest: true
 }
