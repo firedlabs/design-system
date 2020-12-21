fae56dc42e564023192323ac4ac98cefe0366eff
Marco Bruno <marco.bruno.br@gmail.com>
2020-12-21 01:18:32 +0100

    :bookmark: Version 0.2.22

--- dist/components/Heading/index.js
+++ dist/components/Heading/index.js
@@ -15,8 +15,18 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
+function _templateObject5() {
+  var data = _taggedTemplateLiteral(["\n  ", ";\n  color: var(--color-third);\n  ", ";\n  text-align: center;\n  margin-bottom: var(--gap-smallest);\n\n  ", ";\n  ", ";\n  ", ";\n  ", ";\n"]);
+
+  _templateObject5 = function _templateObject5() {
+    return data;
+  };
+
+  return data;
+}
+
 function _templateObject4() {
-  var data = _taggedTemplateLiteral(["\n  ", ";\n  color: var(--color-third);\n  ", ";\n  text-align: center;\n  margin-bottom: var(--gap-small);\n\n  ", ";\n  ", ";\n  ", ";\n"]);
+  var data = _taggedTemplateLiteral(["\n    margin-bottom: var(--gap-biggest);\n  "]);
 
   _templateObject4 = function _templateObject4() {
     return data;
@@ -26,7 +36,7 @@ function _templateObject4() {
 }
 
 function _templateObject3() {
-  var data = _taggedTemplateLiteral(["\n    margin-bottom: var(--gap-biggest);\n  "]);
+  var data = _taggedTemplateLiteral(["\n    margin-bottom: var(--gap-big);\n  "]);
 
   _templateObject3 = function _templateObject3() {
     return data;
@@ -36,7 +46,7 @@ function _templateObject3() {
 }
 
 function _templateObject2() {
-  var data = _taggedTemplateLiteral(["\n    margin-bottom: var(--gap-big);\n  "]);
+  var data = _taggedTemplateLiteral(["\n    margin-bottom: var(--gap-medium);\n  "]);
 
   _templateObject2 = function _templateObject2() {
     return data;
@@ -46,7 +56,7 @@ function _templateObject2() {
 }
 
 function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n    margin-bottom: var(--gap-medium);\n  "]);
+  var data = _taggedTemplateLiteral(["\n    margin-bottom: var(--gap-small);\n  "]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -57,22 +67,27 @@ function _templateObject() {
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var hasMedium = function hasMedium(_ref) {
-  var medium = _ref.medium;
-  return medium && (0, _styledComponents.css)(_templateObject());
+var hasSmall = function hasSmall(_ref) {
+  var small = _ref.small;
+  return small && (0, _styledComponents.css)(_templateObject());
 };
 
-var hasBig = function hasBig(_ref2) {
-  var big = _ref2.big;
-  return big && (0, _styledComponents.css)(_templateObject2());
+var hasMedium = function hasMedium(_ref2) {
+  var medium = _ref2.medium;
+  return medium && (0, _styledComponents.css)(_templateObject2());
 };
 
-var hasBiggest = function hasBiggest(_ref3) {
-  var biggest = _ref3.biggest;
-  return biggest && (0, _styledComponents.css)(_templateObject3());
+var hasBig = function hasBig(_ref3) {
+  var big = _ref3.big;
+  return big && (0, _styledComponents.css)(_templateObject3());
 };
 
-var Heading = _styledComponents.default.h1(_templateObject4(), _Tools.Heading, _Tools.TextShadow.small, hasMedium, hasBig, hasBiggest);
+var hasBiggest = function hasBiggest(_ref4) {
+  var biggest = _ref4.biggest;
+  return biggest && (0, _styledComponents.css)(_templateObject4());
+};
+
+var Heading = _styledComponents.default.h1(_templateObject5(), _Tools.Heading, _Tools.TextShadow.small, hasSmall, hasMedium, hasBig, hasBiggest);
 
 var _default = Heading;
 exports.default = _default;
--- dist/components/Title/index.js
+++ dist/components/Title/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -7,12 +9,64 @@ exports.default = void 0;
 
 var _index = require("../../Styles/Tools/index");
 
-var _styledComponents = _interopRequireDefault(require("styled-components"));
+var _styledComponents = _interopRequireWildcard(require("styled-components"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _templateObject6() {
+  var data = _taggedTemplateLiteral(["\n  ", ";\n  color: var(--color-third);\n  margin-bottom: var(--gap-smallest);\n\n  ", ";\n  ", ";\n  ", ";\n  ", ";\n  ", ";\n"]);
+
+  _templateObject6 = function _templateObject6() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject5() {
+  var data = _taggedTemplateLiteral(["\n    margin-bottom: var(--gap-biggest);\n  "]);
+
+  _templateObject5 = function _templateObject5() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject4() {
+  var data = _taggedTemplateLiteral(["\n    margin-bottom: var(--gap-big);\n  "]);
+
+  _templateObject4 = function _templateObject4() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject3() {
+  var data = _taggedTemplateLiteral(["\n    margin-bottom: var(--gap-medium);\n  "]);
+
+  _templateObject3 = function _templateObject3() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject2() {
+  var data = _taggedTemplateLiteral(["\n    margin-bottom: var(--gap-small);\n  "]);
+
+  _templateObject2 = function _templateObject2() {
+    return data;
+  };
+
+  return data;
+}
 
 function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n  ", ";\n  color: ", ";\n"]);
+  var data = _taggedTemplateLiteral(["\n    color: var(--color-black-first);\n  "]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -23,10 +77,32 @@ function _templateObject() {
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var Title = _styledComponents.default.h2(_templateObject(), _index.Title, function (_ref) {
+var hasDark = function hasDark(_ref) {
   var dark = _ref.dark;
-  return dark ? 'var(--color-black-first)' : 'var(--color-third)';
-});
+  return dark && (0, _styledComponents.css)(_templateObject());
+};
+
+var hasSmall = function hasSmall(_ref2) {
+  var small = _ref2.small;
+  return small && (0, _styledComponents.css)(_templateObject2());
+};
+
+var hasMedium = function hasMedium(_ref3) {
+  var medium = _ref3.medium;
+  return medium && (0, _styledComponents.css)(_templateObject3());
+};
+
+var hasBig = function hasBig(_ref4) {
+  var big = _ref4.big;
+  return big && (0, _styledComponents.css)(_templateObject4());
+};
+
+var hasBiggest = function hasBiggest(_ref5) {
+  var biggest = _ref5.biggest;
+  return biggest && (0, _styledComponents.css)(_templateObject5());
+};
+
+var Title = _styledComponents.default.h2(_templateObject6(), _index.Title, hasDark, hasSmall, hasMedium, hasBig, hasBiggest);
 
 var _default = Title;
 exports.default = _default;
