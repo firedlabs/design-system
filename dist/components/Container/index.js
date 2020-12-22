69934fac60bb6da3210c8cd2b158a42f09cc7c0d
Marco Bruno <marco.bruno.br@gmail.com>
2020-12-22 22:24:15 +0100

    :bookmark: Version 0.2.38

--- dist/components/Container/index.js
+++ dist/components/Container/index.js
@@ -9,10 +9,14 @@ var _styledComponents = _interopRequireDefault(require("styled-components"));
 
 var _Tools = require("../../Styles/Tools");
 
+var _styles = require("../ContentDay/styles");
+
+var _styles2 = _interopRequireDefault(require("../ListContentDay/styles"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n  ", ";\n"]);
+  var data = _taggedTemplateLiteral(["\n  ", ";\n\n  & > ", " {\n    @media (max-width: 1290px) {\n      flex-direction: column;\n\n      & > ", " {\n        width: 100%;\n      }\n    }\n  }\n"]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -23,7 +27,7 @@ function _templateObject() {
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var Container = _styledComponents.default.div(_templateObject(), _Tools.Container);
+var Container = _styledComponents.default.div(_templateObject(), _Tools.Container, _styles2.default, _styles.ContentDayStyle);
 
 var _default = Container;
 exports.default = _default;
