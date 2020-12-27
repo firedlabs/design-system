7116678e5008f50afc79afacd1381aa4fae21333
Marco Bruno <marco.bruno.br@gmail.com>
2020-12-27 13:31:59 +0100

    :bookmark: Version 0.2.44

--- dist/components/ActionImage/index.js
+++ dist/components/ActionImage/index.js
@@ -11,24 +11,32 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 
 var _styles = require("./styles");
 
+var _palette = require("../../Styles/Settings/Colors/palette");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function ActionImage(_ref) {
   var href = _ref.href,
       src = _ref.src,
-      alt = _ref.alt;
-  return /*#__PURE__*/_react.default.createElement(_styles.Action, {
-    href: href
+      alt = _ref.alt,
+      backgroundColor = _ref.backgroundColor;
+  return /*#__PURE__*/_react.default.createElement(_styles.ActionImageStyle, {
+    href: href,
+    backgroundColor: backgroundColor
   }, /*#__PURE__*/_react.default.createElement(_styles.Image, {
     src: src,
     alt: alt
   }));
 }
 
+ActionImage.defaultProps = {
+  backgroundColor: _palette.palette.colorFirst
+};
 ActionImage.propTypes = {
   href: _propTypes.default.string.isRequired,
   src: _propTypes.default.string.isRequired,
-  alt: _propTypes.default.string.isRequired
+  alt: _propTypes.default.string.isRequired,
+  backgroundColor: _propTypes.default.string
 };
 var _default = ActionImage;
 exports.default = _default;
--- dist/components/ActionImage/styles.js
+++ dist/components/ActionImage/styles.js
@@ -1,18 +1,34 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Action = exports.Image = void 0;
+exports.ActionImageStyle = exports.Image = void 0;
 
-var _styledComponents = _interopRequireDefault(require("styled-components"));
+var _styledComponents = _interopRequireWildcard(require("styled-components"));
+
+var _palette = require("../../Styles/Settings/Colors/palette");
 
 var _Tools = require("../../Styles/Tools");
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
+function _templateObject3() {
+  var data = _taggedTemplateLiteral(["\n  display: inline-block;\n  cursor: pointer;\n  width: 20rem;\n  ", ";\n  background-color: var(--color-first);\n  ", ";\n\n  &:hover {\n    transform: scale(1.1);\n  }\n"]);
+
+  _templateObject3 = function _templateObject3() {
+    return data;
+  };
+
+  return data;
+}
 
 function _templateObject2() {
-  var data = _taggedTemplateLiteral(["\n  display: inline-block;\n  cursor: pointer;\n  width: 20rem;\n  ", ";\n\n  &:hover {\n    transform: scale(1.1);\n  }\n"]);
+  var data = _taggedTemplateLiteral(["\n  width: 100%;\n"]);
 
   _templateObject2 = function _templateObject2() {
     return data;
@@ -22,7 +38,7 @@ function _templateObject2() {
 }
 
 function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n  width: 100%;\n"]);
+  var data = _taggedTemplateLiteral(["\n    background-color: ", ";\n  "]);
 
   _templateObject = function _templateObject() {
     return data;
@@ -33,10 +49,15 @@ function _templateObject() {
 
 function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var Image = _styledComponents.default.img(_templateObject());
+var changeColor = function changeColor(_ref) {
+  var backgroundColor = _ref.backgroundColor;
+  return backgroundColor && (0, _styledComponents.css)(_templateObject(), _palette.palette[backgroundColor]);
+};
+
+var Image = _styledComponents.default.img(_templateObject2());
 
 exports.Image = Image;
 
-var Action = _styledComponents.default.a(_templateObject2(), _Tools.BoxShadow.medium);
+var ActionImageStyle = _styledComponents.default.a(_templateObject3(), _Tools.BoxShadow.medium, changeColor);
 
-exports.Action = Action;
+exports.ActionImageStyle = ActionImageStyle;
