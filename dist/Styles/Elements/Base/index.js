17445e32d298ba486645a84cb9fac25b4d1d4dcb
Marco Bruno <marco.bruno.br@gmail.com>
2020-12-28 12:55:44 +0100

    :bookmark: Version 0.2.52

--- dist/Styles/Elements/Base/index.js
+++ dist/Styles/Elements/Base/index.js
@@ -10,7 +10,7 @@ var _styledComponents = require("styled-components");
 var _Tools = require("../../Tools");
 
 function _templateObject() {
-  var data = _taggedTemplateLiteral(["\n  html, body {\n    ", ";\n    font-family: 'Fira Code', sans-serif;\n    font-size: 10px;\n    min-width: 290px;\n\n    @media(max-width: 834px) {\n      font-size: 7px\n    }\n\n    @media(max-width: 510px) {\n      font-size: 6px;\n    }\n\n    @media(max-width: 430px) {\n      font-size: 5px;\n    }\n\n    @media(max-width: 360px) {\n      font-size: 4px;\n    }\n  }\n"]);
+  var data = _taggedTemplateLiteral(["\n  html, body {\n    ", ";\n    font-family: 'Fira Code', sans-serif;\n    font-size: 10px;\n    min-width: 290px;\n    background-color: var(--color-black-second);\n\n    @media(max-width: 834px) {\n      font-size: 7px\n    }\n\n    @media(max-width: 510px) {\n      font-size: 6px;\n    }\n\n    @media(max-width: 430px) {\n      font-size: 5px;\n    }\n\n    @media(max-width: 360px) {\n      font-size: 4px;\n    }\n  }\n"]);
 
   _templateObject = function _templateObject() {
     return data;
