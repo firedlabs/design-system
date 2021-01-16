50f5b3e1f91aabb59c28f6d20c81185d6ae68937
Marco Bruno <marco.bruno.br@gmail.com>
2021-01-16 17:51:26 +0100

    :bookmark: Version 0.3.9

---
+++ dist/components/Field/Text/index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _styles = require("../styles");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function Text(_ref) {
+  var value = _ref.value,
+      type = _ref.type,
+      label = _ref.label,
+      placeholder = _ref.placeholder,
+      readOnly = _ref.readOnly;
+  return /*#__PURE__*/_react.default.createElement(_styles.Label, null, /*#__PURE__*/_react.default.createElement(_styles.Content, null, label), /*#__PURE__*/_react.default.createElement(_styles.Input, {
+    value: value,
+    type: type,
+    placeholder: placeholder,
+    readOnly: readOnly
+  }));
+}
+
+Text.defaultProps = {
+  value: '',
+  type: 'text',
+  label: '',
+  placeholder: '',
+  readOnly: false
+};
+Text.propTypes = {
+  value: _propTypes.default.string,
+  type: _propTypes.default.string,
+  label: _propTypes.default.string,
+  placeholder: _propTypes.default.string,
+  readOnly: _propTypes.default.bool
+};
+var _default = Text;
+exports.default = _default;
--- dist/components/Field/index.js
+++ dist/components/Field/index.js
@@ -7,10 +7,13 @@ exports.default = void 0;
 
 var _Email = _interopRequireDefault(require("./Email"));
 
+var _Text = _interopRequireDefault(require("./Text"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Field = {
-  Email: _Email.default
+  Email: _Email.default,
+  Text: _Text.default
 };
 var _default = Field;
 exports.default = _default;
